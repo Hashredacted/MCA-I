8b57d33089becad6454ca1197ecdf5faf57d5d81
Hashredacted <mohammadafnanmirza@gmail.com>
2024-09-09 10:21:06 +0530

    Week 4 uploaded

---
+++ WEEK 4/#1 OddOrEven.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+//	string num;
+	cout<<"Enter a number ";
+	cin>>a;
+//	num = 
+	(a%2==0)?(cout<<a<<" is even"):(cout<<a<<" is odd");
+//	cout<<a<<num;
+	return 0;
+}
